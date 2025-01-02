8af3d7d9ed2160ad6913a49a1ea96ed6c0372fd6
Sergio Marcelo Hosan <marcelo.hosan@arba.gov.ar>
2025-01-02 17:32:41 -0300

    desktop

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,10 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit  } from '@angular/core';
 import * as L from 'leaflet';
 import 'leaflet.markercluster';
-
-//import 'leaflet.markercluster/dist/leaflet.markercluster.js';
-//import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
-//import 'leaflet.markercluster/dist/MarkerCluster.css';
 import { DataFromCSVService } from '../../services/data-from-csv.service';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -16,22 +12,45 @@ import { HttpClientModule } from '@angular/common/http';
     templateUrl: './mapa.component.html',
     styleUrl: './mapa.component.css'
 })
-export class MapaComponent implements OnInit {
+export class MapaComponent implements OnInit, AfterViewInit  {
   private map!: L.Map;
-  private layerGroup!: L.LayerGroup;
+  //private layerGroup!: L.LayerGroup;
   private markerClusterGroup!: L.MarkerClusterGroup;
 
   constructor(private dataFromCSVService: DataFromCSVService) { }
 
   ngOnInit(): void {
-    this.initMap();
     this.dataFromCSVService.getCSVData().subscribe(data => {
-      //console.log(data[0].latitud, data[0].longitud);
       this.addPointsToMap(data);
     });
   }
+  
+  ngAfterViewInit() {
+    this.initMap();
+  }
 
   private initMap(): void {
+  // Sobrescribir el método on de L.DomEvent para usar listeners pasivos
+  const originalOn = L.DomEvent.on;
+  L.DomEvent.on = function(
+    el: HTMLElement,
+    types: string | { [eventName: string]: L.DomEvent.EventHandlerFn },
+    fn?: L.DomEvent.EventHandlerFn | any,
+    context?: any
+  ): typeof L.DomEvent {
+    if (typeof types === 'string' && typeof fn === 'function') {
+      const passiveTypes = ['touchstart', 'touchmove', 'wheel', 'mousewheel'];
+      if (passiveTypes.some(type => types.indexOf(type) !== -1)) {
+        types.split(' ').forEach(type => {
+          el.addEventListener(type, fn, { passive: true });
+        });
+        return L.DomEvent;
+      }
+    }
+    // Usar apply para pasar los argumentos correctamente
+    return originalOn.apply(L.DomEvent, arguments as any);
+  };
+ 
     const buenosAiresCenter: L.LatLngTuple = [-36.6167, -60.7000];
     this.map = L.map('map').setView(buenosAiresCenter, 5);
 
