dec572720cd4c3f4a5e538ba6c26c7c79e40cba4
Sergio Marcelo Hosan <marcelo.hosan@arba.gov.ar>
2025-01-02 14:13:37 -0300

    carga de datos con markerClustering

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
+import 'leaflet.markercluster';
+import * as LMC from 'leaflet.markercluster';
+import 'leaflet.markercluster/dist/leaflet.markercluster.js';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
 import { DataFromCSVService } from '../../services/data-from-csv.service';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -14,6 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
 export class MapaComponent implements OnInit {
   private map!: L.Map;
   private layerGroup!: L.LayerGroup;
+  private markerClusterGroup!: L.MarkerClusterGroup;
 
   constructor(private dataFromCSVService: DataFromCSVService) { }
 
@@ -27,18 +33,20 @@ export class MapaComponent implements OnInit {
 
   private initMap(): void {
     const buenosAiresCenter: L.LatLngTuple = [-36.6167, -60.7000];
-    this.map = L.map('map').setView(buenosAiresCenter, 4);
+    this.map = L.map('map').setView(buenosAiresCenter, 5);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: '© OpenStreetMap'
     }).addTo(this.map);
 
-    this.layerGroup = L.layerGroup().addTo(this.map);
+    //this.layerGroup = L.layerGroup().addTo(this.map);
+    this.markerClusterGroup = L.markerClusterGroup();
+    this.map.addLayer(this.markerClusterGroup);
   }
 
   private addPointsToMap(data: any[]): void {
-    const batchSize = 10000; // Tamaño del lote
+    const batchSize = 30000; // Tamaño del lote
     let index = 0;
 
     const addBatch = () => {
@@ -47,20 +55,21 @@ export class MapaComponent implements OnInit {
         const lat = parseFloat(point.latitud);
         const lng = parseFloat(point.longitud);
         if (!isNaN(lat) && !isNaN(lng)) {
-          L.circleMarker([lat, lng], {
-            radius: 3,
+          const marker = L.circleMarker([lat, lng], {
+            radius: 2,
             color: 'blue',
             fillColor: '#30f',
             fillOpacity: 0.5
-          }).bindPopup(`Latitud: ${lat}, Longitud: ${lng}`).addTo(this.layerGroup);
-       }
+          }).bindPopup(`Latitud: ${lat}, Longitud: ${lng}`);
+          this.markerClusterGroup.addLayer(marker);
+        }
       });
       index += batchSize;
-      alert(`Lote de ${batchSize} puntos agregado. ${index}`); // Alert al finalizar cada lote
+      //alert(`Lote de ${batchSize} puntos agregado. ${index}`); // Alert al finalizar cada lote
       if (index < data.length) {
         setTimeout(addBatch, 100); // Espera 100ms antes de agregar el siguiente lote
       } else {
-        alert('Todos los puntos han sido agregados.'); // Alert final al terminar todos los puntos
+        //alert('Todos los puntos han sido agregados.'); // Alert final al terminar todos los puntos
       }
     };
 
