06382136e083069b32651b19af39996520017507
Marcelo Hosan <mhosan@gmail.com>
2025-01-03 17:20:15 -0300

    dos componentes nuevos: mapaOpenlayers y home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MapaLeafletComponent } from "./components/mapaLeaflet/mapaLeaflet.component";
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, HttpClientModule, MapaLeafletComponent]
+    imports: [CommonModule, HttpClientModule, HomeComponent]
 })
 export class AppComponent {
   title = 'visoria';
---
+++ src/app/components/mapaOpenlayers/mapaOpenlayers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapa-openlayers',
+  standalone: true,
+  imports: [],
+  templateUrl: './mapaOpenlayers.component.html',
+  styleUrl: './mapaOpenlayers.component.css'
+})
+export class MapaOpenlayersComponent {
+
+}
