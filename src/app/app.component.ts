2e2eaec19db7b91ee8802b462f891b66cc2e65db
Marcelo Hosan <mhosan@gmail.com>
2025-01-03 20:52:39 -0300

    ajustando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+//import { HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/mapaLeaflet/mapaLeaflet.component.ts
+++ src/app/components/mapaLeaflet/mapaLeaflet.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit, AfterViewInit  } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 //import 'leaflet.markercluster';
 import { DataFromCSVService } from '../../services/data-from-csv.service';
-import { HttpClientModule } from '@angular/common/http';
 
 
 @Component({
-    selector: 'app-mapa-leaflet',
-    standalone: true,
-    imports: [HttpClientModule],
-    templateUrl: './mapaLeaflet.component.html',
-    styleUrl: './mapaLeaflet.component.css'
+  selector: 'app-mapa-leaflet',
+  standalone: true,
+  imports: [],
+  templateUrl: './mapaLeaflet.component.html',
+  styleUrl: './mapaLeaflet.component.css'
 })
-export class MapaLeafletComponent implements OnInit, AfterViewInit  {
+export class MapaLeafletComponent implements OnInit, AfterViewInit {
   private map!: L.Map;
   private layerGroup!: L.LayerGroup;
   //private markerClusterGroup!: L.MarkerClusterGroup;
@@ -24,33 +23,33 @@ export class MapaLeafletComponent implements OnInit, AfterViewInit  {
       this.addPointsToMap(data);
     });
   }
-  
+
   ngAfterViewInit() {
     this.initMap();
   }
 
   private initMap(): void {
-  // Sobrescribir el método on de L.DomEvent para usar listeners pasivos
-  const originalOn = L.DomEvent.on;
-  L.DomEvent.on = function(
-    el: HTMLElement,
-    types: string | { [eventName: string]: L.DomEvent.EventHandlerFn },
-    fn?: L.DomEvent.EventHandlerFn | any,
-    context?: any
-  ): typeof L.DomEvent {
-    if (typeof types === 'string' && typeof fn === 'function') {
-      const passiveTypes = ['touchstart', 'touchmove', 'wheel', 'mousewheel'];
-      if (passiveTypes.some(type => types.indexOf(type) !== -1)) {
-        types.split(' ').forEach(type => {
-          el.addEventListener(type, fn, { passive: true });
-        });
-        return L.DomEvent;
-      }
-    }
-    // Usar apply para pasar los argumentos correctamente
-    return originalOn.apply(L.DomEvent, arguments as any);
-  };
- 
+    /*   // Sobrescribir el método on de L.DomEvent para usar listeners pasivos
+      const originalOn = L.DomEvent.on;
+      L.DomEvent.on = function(
+        el: HTMLElement,
+        types: string | { [eventName: string]: L.DomEvent.EventHandlerFn },
+        fn?: L.DomEvent.EventHandlerFn | any,
+        context?: any
+      ): typeof L.DomEvent {
+        if (typeof types === 'string' && typeof fn === 'function') {
+          const passiveTypes = ['touchstart', 'touchmove', 'wheel', 'mousewheel'];
+          if (passiveTypes.some(type => types.indexOf(type) !== -1)) {
+            types.split(' ').forEach(type => {
+              el.addEventListener(type, fn, { passive: true });
+            });
+            return L.DomEvent;
+          }
+        }
+        // Usar apply para pasar los argumentos correctamente
+        return originalOn.apply(L.DomEvent, arguments as any);
+      }; */
+
     const buenosAiresCenter: L.LatLngTuple = [-36.6167, -60.7000];
     this.map = L.map('map').setView(buenosAiresCenter, 5);
 
@@ -58,14 +57,14 @@ export class MapaLeafletComponent implements OnInit, AfterViewInit  {
       maxZoom: 19,
       attribution: '© OpenStreetMap'
     }).addTo(this.map);
-    
+
     this.layerGroup = L.layerGroup().addTo(this.map);
     //this.markerClusterGroup = L.markerClusterGroup();
     //this.map.addLayer(this.markerClusterGroup);
   }
 
   private addPointsToMap(data: any[]): void {
-    const batchSize = 30000; // Tamaño del lote
+    const batchSize = 1000; // Tamaño del lote
     let index = 0;
 
     const addBatch = () => {
@@ -84,11 +83,12 @@ export class MapaLeafletComponent implements OnInit, AfterViewInit  {
         }
       });
       index += batchSize;
-      //alert(`Lote de ${batchSize} puntos agregado. ${index}`); // Alert al finalizar cada lote
-      if (index < data.length) {
+      alert(`Lote de ${batchSize} puntos agregado. ${index}`); // Alert al finalizar cada lote
+      //if (index < data.length) {
+        if (index < 2100) {
         setTimeout(addBatch, 100); // Espera 100ms antes de agregar el siguiente lote
       } else {
-        //alert('Todos los puntos han sido agregados.'); // Alert final al terminar todos los puntos
+        alert('Todos los puntos han sido agregados.'); // Alert final al terminar todos los puntos
       }
     };
 
